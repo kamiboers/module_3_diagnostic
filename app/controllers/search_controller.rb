5b0725e138c5f4658a98e39611dba634c207a9fd
Kami Boers <kamiboers@gmail.com>
2016-06-06 10:11:14 -0600

    nope

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,11 @@ class SearchController < ApplicationController
 
 def index
   zip = params[:zip]
+  connection = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations")
   binding.pry
-  connection = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=10&api_key=#{NREL_KEY}")
-  response = connection.get "/nearest", {format: 'json', location: zip}
+  response = HTTParty.get('http://api.stackexchange.com/2.2/questions?site=stackoverflow')
+  response = connection.get "/nearest", {format: 'json', location: zip, api_key: ENV['NREL_KEY']}
+  binding.pry
   response.body
 end
 
