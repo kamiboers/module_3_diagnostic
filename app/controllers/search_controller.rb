51eacbf500afbf3fffb166b0df33eb8f8b748add
Kami Boers <kamiboers@gmail.com>
2016-06-06 09:57:32 -0600

    faraday connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@ class SearchController < ApplicationController
 
 def index
   zip = params[:zip]
-  connection = Faraday.new(url: "")
-  response = connection.get ""
   binding.pry
+  connection = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=10&api_key=#{NREL_KEY}")
+  response = connection.get "/nearest", {format: 'json', location: zip}
   response.body
 end
 
